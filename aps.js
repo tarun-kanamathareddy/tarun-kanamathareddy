a7741721510bb468849ee96ef77e5ac97de295c6
tarun-kanamathareddy <saitarun722@gmail.com>
2022-07-26 11:50:04 +0530

    modified:   aps.js

--- aps.js
+++ aps.js
@@ -1,62 +1,62 @@
 
 
-// function getcapture(camera){
-//     camera();
-// }
-// getcapture(function(){
-//     console.log('canon');
-// });
-// <------------------higher order functions---------------->
-// function func1(){
-//     return function(){
-//         console.log('hi');
-//     }
-// }
-// const fn = func1();
-// fn();
+function getcapture(camera){
+    camera();
+}
+getcapture(function(){
+    console.log('canon');
+});
+<------------------higher order functions---------------->
+function func1(){
+    return function(){
+        console.log('hi');
+    }
+}
+const fn = func1();
+fn();
 
 
 
-// function test(name){
-//     if(test){
-//         return function(topic){
-//             console.log(`hi this is ${name} and i am here to give test of ${topic}`);
-//         }
-//     }
-//     else{
-//         return function(){
-//             console.log("SOrry you cant attend the test")
-//         }
-//     }
-// }
-// console.log("check")
-// function trigger(){
+function test(name){
+    if(test){
+        return function(topic){
+            console.log(`hi this is ${name} and i am here to give test of ${topic}`);
+        }
+    }
+    else{
+        return function(){
+            console.log("SOrry you cant attend the test")
+        }
+    }
+}
+console.log("check")
+function trigger(){
 
-//     const fn = test("rahul");
-// fn("art");
-// fn("js");
+    const fn = test("rahul");
+fn("art");
+fn("js");
 
-// fn("ui");
+fn("ui");
 
-// fn("ux");
-//     // setInterval(fn,3000)
-// }
-// trigger()
-// test("rahul")("java");
-// const fn = test("rahul");
-// fn("art");
-// fn("js");
+fn("ux");
+    // setInterval(fn,3000)
+}
+trigger()
+test("rahul")("java");
+const fn = test("rahul");
+fn("art");
+fn("js");
 
-// fn("ui");
+fn("ui");
 
-// fn("ux");
+fn("ux");
 
 
 
-// <!!!----------------------setTimeout()--------------------------!!!>
+<!!!----------------------setTimeout()--------------------------!!!>
 
-// function tarun(){
-//     console.log('goodmorning team')
-// }
-// setTimeout(trigger,3000)
-// setInterval(trigger,3000)
+function tarun(){
+    console.log('goodmorning team')
+}
+setTimeout(trigger,3000)
+setInterval(trigger,3000)
